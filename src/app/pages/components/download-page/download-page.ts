40c1477c38fe469f1813c6ce88799a860d211bef
NothingAroundUs <husseintaha756@gmail.com>
2026-02-06 07:13:08 -0800

    minor bug fixes related to downloads

--- src/app/pages/components/download-page/download-page.ts
+++ src/app/pages/components/download-page/download-page.ts
@@ -1,21 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-download-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './download-page.html',
   styleUrl: './download-page.css',
 })
 export class DownloadPage {
+  version: string | null = '';
+
   constructor(private route: ActivatedRoute) {}
-  versions: string | null = ''
 
   ngOnInit() {
     const version = this.route.snapshot.paramMap.get('version');
-    this.versions = version
+    this.version = version;
+
     if (version) {
-      window.location.href = `https://egtfb.vercel.app/versions/${version}/EFB.apk`;
+      this.forceDownload(version);
     }
+  }
+
+  forceDownload(version: string | null) {
+    if (!version) return;
+
+    const url = `https://egtfb.vercel.app/versions/${version}/EFB.apk`;
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.setAttribute('download', '');
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   }
 }
